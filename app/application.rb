5a1143716a9325141b11988ffd8e697b7c5b086f
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-05 15:56:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,10 +9,12 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
-    elsif item == nil
-      resp.write "Item not found"
-      resp.status = 400
+      if item != nil
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
