f63ebf2c4022e0bb541c7f11784d0125a75770ba
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-05 15:51:32 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name}
+      resp.write item.name
+    else
+      resp.write "Item not found"
+      resp.status = 400
+    end
+    resp.finish
+  end
+
+end
